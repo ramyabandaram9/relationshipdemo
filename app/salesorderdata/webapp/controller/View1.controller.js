daf2bf53a3b3eafd76601077fcc315af329025c2
Ramya Bandaram <ramyabandaram9@gmail.com>
2026-03-11 10:58:56 +0000

    updated functions

--- app/salesorderdata/webapp/controller/View1.controller.js
+++ app/salesorderdata/webapp/controller/View1.controller.js
@@ -24,6 +24,42 @@ sap.ui.define([
             }catch(error){
                  sap.m.MessageToast.show('Failed');
             }
+        },
+        async onItemPress(oEvent){
+            var ordid = oEvent.getParameter('listItem').getBindingContext().getObject().ID;
+
+            var oModel = this.getOwnerComponent().getModel();
+            var oList = oModel.bindContext(`/SalesOrderHeaderSet(ID=${ordid})`,null,{
+                $expand : 'item_details_comp'
+            });
+            var oCtx = await oList.requestObject();
+            this.getView().setModel(new sap.ui.model.json.JSONModel(oCtx.item_details_comp),"soitems")
+        },
+
+        async onOrderTotal(){
+            const oTable =this.byId('salesDesTabId');
+            const oItem = oTable.getSelectedItem();
+            if(!oItem){
+                sap.m.MessageToast.show('Select an Order first');
+                return;
+            }
+
+            const oCtx = oItem.getBindingContext();
+            const sOrderId = oCtx.getProperty('ID');
+
+            const oModel = this.getView().getModel();
+            const oFunction = oModel.bindContext(
+                `/getOrderTotal(orderId='${sOrderId}')`
+            );
+            try{
+                const oResultsCtx = await oFunction.requestObject();
+                const oTotal = oResultsCtx.value;
+
+                this.byId('totalTXTId').setText(`Total: $${oTotal}`)
+
+            }catch(error){
+                sap.m.MessageToast.show(error.message || "Failed to get total")
+            }
         }
     });
 });
